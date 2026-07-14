f44271740e3c588d4cef5c800d09ca83636e9070
Olga Sergeenkova <o.y.sergeenkova@gmail.com>
2026-07-14 11:59:45 +0200

    add exercise React Router part 1

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { NavLink, Outlet } from "react-router-dom";
+import styles from "./components/Site.module.css";
+import { S } from "./components/pages/_styles";
+
+const PATH = {
+  PAGE1: "/adidas",
+  PAGE2: "/puma",
+  PAGE3: "/abibas",
+  PAGE4: "/prices",
+  PAGE5: "/protected",
+} as const;
+
+export const App = () => {
+  return (
+    <div>
+      <div className={styles.header}>
+        <h1>HEADER</h1>
+      </div>
+      <div className={styles.body}>
+        <div className={styles.nav}>
+          <S.NavWrapper>
+            <NavLink to={PATH.PAGE1}>Adidas</NavLink>
+          </S.NavWrapper>
+          <S.NavWrapper>
+            <NavLink to={PATH.PAGE2}>Puma</NavLink>
+          </S.NavWrapper>
+          <S.NavWrapper>
+            <NavLink to={PATH.PAGE3}>Abibas</NavLink>
+          </S.NavWrapper>
+          <S.NavWrapper>
+            <NavLink to={PATH.PAGE4}>Цены для оптовиков</NavLink>
+          </S.NavWrapper>
+          <S.NavWrapper>
+            <NavLink to={PATH.PAGE5}>ProtectedPage</NavLink>
+          </S.NavWrapper>
+        </div>
+        <div className={styles.content}>
+          <Outlet />
+          {/* <Routes> */}
+          {/* <Route path={"/"} element={<Navigate to={PATH.PAGE1} />} />
+
+            <Route path={PATH.PAGE1} element={<Adidas />} />
+            <Route path={PATH.PAGE2} element={<Puma />} />
+            <Route path={PATH.PAGE3} element={<Abibas />} />
+            <Route path={PATH.PAGE4} element={<Prices />} /> */}
+
+          {/*<Route path={'/adidas/:id'} element={<Model/>}/>*/}
+          {/* <Route path={"/:model/:id"} element={<Model />} />
+
+            <Route path={"/*"} element={<Error404 />} /> */}
+
+          {/*<Route path={'/page/error'} element={<Error404/>}/>*/}
+          {/*<Route path={'/*'} element={<Navigate to={'/page/error'}/>}/>*/}
+          {/* </Routes> */}
+        </div>
+      </div>
+      <div className={styles.footer}>abibas 2023</div>
+    </div>
+  );
+};
---
+++ src/routes/router.tsx
@@ -0,0 +1,67 @@
+import { createBrowserRouter } from "react-router-dom";
+import { App } from "../App";
+import { Abibas } from "../components/pages/Abibas";
+import { Adidas } from "../components/pages/Adidas";
+import { Error404 } from "../components/pages/Error404";
+import { Model } from "../components/pages/Model";
+import { Prices } from "../components/pages/Prices";
+import { ProtectedPage } from "../components/pages/ProtectedPage";
+import { Puma } from "../components/pages/Puma";
+import { ProtectedRoute } from "./ProtectedRoute";
+
+const PATH = {
+  ADIDAS: "adidas",
+  PUMA: "puma",
+  ABIBAS: "abibas",
+  PRICE: "prices",
+  MODEL: "/:model/:id",
+  PROTECTEDPAGE: "protected",
+  ERROR: "/error",
+  GLOBALERROR: "/*",
+} as const;
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error404 />,
+    children: [
+      {
+        path: PATH.ADIDAS,
+        element: <Adidas />,
+      },
+      {
+        path: PATH.PUMA,
+        element: <Puma />,
+      },
+      {
+        path: PATH.ABIBAS,
+        element: <Abibas />,
+      },
+      {
+        path: PATH.PRICE,
+        element: <Prices />,
+      },
+      {
+        path: PATH.MODEL,
+        element: <Model />,
+      },
+      {
+        path: PATH.ERROR,
+        element: <Error404 />,
+      },
+      {
+        path: PATH.GLOBALERROR,
+        element: <Error404 />,
+      },
+      {
+        path: PATH.PROTECTEDPAGE,
+        element: (
+          <ProtectedRoute>
+            <ProtectedPage />
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
+]);
